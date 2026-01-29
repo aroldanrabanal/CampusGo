599767495e684b8efdadadb1bb80bcff08520095
angel clase <aroldanrabanal@safareyes.es>
2026-01-29 13:00:09 +0100

    proxy

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.campusgo.app',
   appName: 'CampusGo',
-  webDir: './dist/campusgo',
+  webDir: 'www',
 };
 
 export default config;
